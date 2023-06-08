3a112f70ac47faf6b1b219ff259e57d18b0d189c
Pedro-Henrique-da-Silva-Santos <pedro.santos.1429140@sga.pucminas.br>
2023-06-08 10:38:30 -0300

    Desenvolvimento da Home-Page (Parte I)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import './components/Layout/index.scss';
+import './components/Home/index.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -1,13 +1,11 @@
 import { Link } from 'react-router-dom';
-import './index.scss'
+import "./index.scss";
 
 const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Olá, <br /> Meu nome é <span>Lucas</span> <br /> e eu sou <span>Desenvolvedor Front-End</span> <br /> e <span>UI Designer</span> <br />
-                    <img></img>
-                </h1>
+                <h1>Olá, eu sou o <span className="name">Pedro</span></h1>               
                 <h2>Desenvolvedor Frontend</h2>
                 <Link to="/contato" className="flat-button">Contate-me</Link>
             </div>
@@ -15,4 +13,4 @@ const Home = () => {
     );
 };
 
-export default Home;
+export default Home
