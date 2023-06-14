7eb24f6bfcb486922963939b8fb36b12369aa30b
Pedro-Henrique-da-Silva-Santos <pedro.santos.1429140@sga.pucminas.br>
2023-06-14 16:49:18 -0300

    Formulário de Contato + Integração com o EmailJs

--- my-app/src/components/Contato/index.js
+++ my-app/src/components/Contato/index.js
@@ -2,16 +2,36 @@ import './index.scss';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contato = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate');
+    const refForm = useRef();
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
-        }, 4000)
+        }, 3000)
     }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        
+        emailjs.sendForm('', '', refForm.current, '')
+            .then(
+                () => {
+                    alert('E-mail enviado com sucesso!');
+                    window.location.reload(false);
+                },
+                () => {
+                    alert('Erro ao enviar e-mail. Por favor tente novamente!');
+
+                }
+            )
+    }
 
     return(
         <>
@@ -24,19 +44,19 @@ const Contato = () => {
                         eu sou um parágrafo de teste
                     </p>
                     <div className='form-contato'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='campos'>
-                                    <input type='text' placeholder='Nome' name='nome' required/>
+                                    <input type='text' placeholder='Nome' name='from_name' required/>
                                 </li>                                
                                 <li className='campos'>
-                                    <input type='email' placeholder='E-mail' name='email' required/>
+                                    <input type='email' placeholder='E-mail' name='from_email' required/>
                                 </li>                                
                                 <li>
                                     <input type='text' placeholder='Assunto' name='assunto' required/>
                                 </li>                               
                                 <li>
-                                    <textarea placeholder='Mensagem' name='mensagem' required></textarea>
+                                    <textarea placeholder='Mensagem' name='message' required></textarea>
                                 </li>
                                 <li>
                                     <input type='submit' value='Enviar' className='flat-button'/>
