1abf1e901b8e20cb06b6ad89837e94a027f1b4db
Pedro-Henrique-da-Silva-Santos <pedro.santos.1429140@sga.pucminas.br>
2023-08-28 17:40:12 -0300

    update:home-page | some desktop responsiveness adjusts & start of mobile responsiveness

--- my-app/src/components/Projetos/index.js
+++ my-app/src/components/Projetos/index.js
@@ -26,8 +26,8 @@ const Projetos = () => {
                     <h1 >
                         <AnimatedLetters letterClass={letterClass} strArray={['P', 'r', 'o', 'j', 'e', 't', 'o', 's']} idx={15}></AnimatedLetters>
                     </h1>
-                    <div className='paragrafo-yourmoney'>
-                        <a href='https://your-money20230609182318.azurewebsites.net/'><h4>Acesso ao site aqui!</h4><FontAwesomeIcon icon={faArrowUpRightFromSquare} className='site-link' /></a>
+                    <div className='desc-projetos'>
+                        <a target='_blank' rel='noreferrer' href='https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-1-e2-proj-int-t7-your-money'><h4>Link do Repositório!</h4><FontAwesomeIcon icon={faArrowUpRightFromSquare} className='site-link' /></a>
                         <p>
                             <h3>YourMoney</h3>
                             Nesta sessão quero mostrar os dois projetos que desenvolvi junto a meus colegas de curso na faculdade.
@@ -39,8 +39,8 @@ const Projetos = () => {
                             estruturar todo o Backlog e o próprio GitHub para versionamento de código.
                         </p>
                     </div>
-                    <div className='paragrafo-procurandoalguem'>
-                        <a href='https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2022-2-e1-proj-web-t5-procurando-alguem/src/paginas/usuario/home-page.html'><h4>Acesso ao site aqui!</h4><FontAwesomeIcon icon={faArrowUpRightFromSquare} className='site-link' /></a>
+                    <div className='desc-projetos'>
+                        <a target='_blank' rel='noreferrer' href='https://icei-puc-minas-pmv-ads.github.io/pmv-ads-2022-2-e1-proj-web-t5-procurando-alguem/src/paginas/usuario/home-page.html'><h4>Acesso ao site aqui!</h4><FontAwesomeIcon icon={faArrowUpRightFromSquare} className='site-link' /></a>
                         <p>
                             <h3>Procurando Alguem?</h3>
                             O segundo projeto é uma aplicação web que busca oferecer um ambiente simples e intuitivo para auxiliar na busca
@@ -60,7 +60,7 @@ const Projetos = () => {
                     </div>
 
                     <h1><AnimatedLetters letterClass={letterClass} strArray={['P', 'r', 'o', 'c', 'u', 'r', 'a', 'n', 'd', 'o', '', 'A', 'l', 'g', 'u', 'é', 'm', '?', '', '(', '', 'HTML', '', '|', '', 'CSS', '', '|', '', 'JavaScript', '', ')',]} idx={15}></AnimatedLetters></h1>
-                    <div className='video2'>
+                    <div className='video'>
                         <video src={ProcurandoAlguem} controls={true} poster={Poster}></video>
                     </div>
                 </div>
--- my-app/src/components/Sidebar/index.js
+++ my-app/src/components/Sidebar/index.js
@@ -11,38 +11,37 @@ const Sidebar = () => {
     const [showNav, setShowNav] = useState(false);
 
     return (
-
-        <div className="nav-bar">
-            <Link className='logo' to="/">
-                <img src={Logo} alt="Logo-PH" to='/home' />
-            </Link>
-            <nav className={showNav ? 'mobile-show' : ''}>
-                <NavLink exact='true' activeclassname='active' to='/' onClick={() => setShowNav(false)}>
-                    <FontAwesomeIcon icon={faHome} color='#4d4d4de' />
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="sobre-link" to='/sobre' onClick={() => setShowNav(false)}>
-                    <FontAwesomeIcon icon={faUser} color='#4d4d4de' />
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="contato-link" to='/contato' onClick={() => setShowNav(false)}>
-                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4de' />
-                </NavLink>
-                <NavLink exact='true' activeclassname='active' className="projetos-link" to='/projetos' onClick={() => setShowNav(false)}>
-                    <FontAwesomeIcon icon={faBriefcase} color='#4d4d4de' />
-                </NavLink>
-            </nav>
-            <ul>
-                <li>
-                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/pedrohenriquesilvasantos12032002/'>
-                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4de' />
-                    </a>
-                </li>
-                <li>
-                    <a target='_blank' rel='noreferrer' href='https://github.com/Pedro-Henrique-da-Silva-Santos'>
-                        <FontAwesomeIcon icon={faGithub} color='#4d4d4de' />
-                    </a>
-                </li>
-            </ul>
-        </div>
+            <div className={showNav ? "" : "nav-bar"}>
+                <Link className='logo' to="/">
+                    <img src={Logo} alt="Logo-PH" to='/home' />
+                </Link>
+                <nav className={showNav ? 'mobile-show' : ''}>
+                    <NavLink exact='true' activeclassname='active' to='/' onClick={() => setShowNav(false)}>
+                        <FontAwesomeIcon icon={faHome} color='#4d4d4de' />
+                    </NavLink>
+                    <NavLink exact='true' activeclassname='active' className="sobre-link" to='/sobre' onClick={() => setShowNav(false)}>
+                        <FontAwesomeIcon icon={faUser} color='#4d4d4de' />
+                    </NavLink>
+                    <NavLink exact='true' activeclassname='active' className="contato-link" to='/contato' onClick={() => setShowNav(false)}>
+                        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4de' />
+                    </NavLink>
+                    <NavLink exact='true' activeclassname='active' className="projetos-link" to='/projetos' onClick={() => setShowNav(false)}>
+                        <FontAwesomeIcon icon={faBriefcase} color='#4d4d4de' />
+                    </NavLink>
+                </nav>
+                <ul>
+                    <li>
+                        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/pedrohenriquesilvasantos12032002/'>
+                            <FontAwesomeIcon icon={faLinkedin} color='#4d4d4de' />
+                        </a>
+                    </li>
+                    <li>
+                        <a target='_blank' rel='noreferrer' href='https://github.com/Pedro-Henrique-da-Silva-Santos'>
+                            <FontAwesomeIcon icon={faGithub} color='#4d4d4de' />
+                        </a>
+                    </li>
+                </ul>
+            </div>
     )
 };
 
